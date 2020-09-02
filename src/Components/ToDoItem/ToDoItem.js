90d692dc098e873312e27c0bb75538c1cab9a455
Asaduzzaman Sajeeb <50229812+Asaduzzaman607@users.noreply.github.com>
2020-09-03 00:45:20 +0600

    adding to  staging branch

---
+++ src/Components/ToDoItem/ToDoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToDoItem = () => {
+    return (
+        <div>
+            <h2>This is the starting of to do list items.</h2>
+        </div>
+    );
+};
+
+export default ToDoItem;
